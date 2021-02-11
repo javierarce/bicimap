f6f4245825c4a713c8898470c94661b3d9081e57
Javier Arce <javierarce@users.noreply.github.com>
2021-02-11 18:47:32 +0100

    Fixes cron script

--- public/cron.sh
+++ public/cron.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
-/root/.nvm/versions/node/v12.18.0/bin/node /home/javier/bicimap/stations.js >> /home/javier/bicimap/cron.log
+cd /home/javier/bicimap
+/usr/bin/node /home/javier/bicimap/stations.js >> /home/javier/bicimap/cron.log
