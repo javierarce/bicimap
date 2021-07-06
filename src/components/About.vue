faa4ab35515d3ba04d94643f7f988220da7a5738
Javier Arce <4933+javierarce@users.noreply.github.com>
2021-07-06 11:00:48 +0200

    Adds Nominatin info

--- src/components/About.vue
+++ src/components/About.vue
@@ -14,6 +14,9 @@
             <p>Sigue el desarrollo de este mapa, sugiere mejoras e informa de errores en GitHub.</p>
             <a href="https://github.com/javierarce/bicimap" class="Button About__button is-bold" title="Visita el repositorio en GitHub" target="_blank">Accede al código</a>
           </div>
+          <p class="is-small">Esta web utiliza datos de <a href="https://www.openstreetmap.org/copyright">Nominatim</a>.</p>
+
+
 
 
         <div class="About__footer">
