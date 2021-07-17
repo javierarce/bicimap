b4a3dcfd528bd0b18f16709c599b2cc594e83958
Javier Arce <4933+javierarce@users.noreply.github.com>
2021-07-17 23:59:41 +0200

    Adds border-radius, updates text

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -72,8 +72,8 @@ export default {
         if (results.length) {
           window.bus.$emit(config.ACTIONS.SET_VIEW, results[0])
         } else {
-          let title = config.TEXTS.NO_RESULTS_TITLE.replace('{q}', this.q)
-          let description = config.TEXTS.NO_RESULTS_DESCRIPTION
+          let title = config.TEXTS.NO_RESULTS_TITLE
+          let description = config.TEXTS.NO_RESULTS_DESCRIPTION.replace('{q}', this.q)
           let footer = `<a class="Button Alert__button" href="https://www.openstreetmap.org/search?query=${this.q}#map=${config.MAP.ZOOM}/${config.MAP.LAT}/${config.MAP.LON}" target="__blank">Añade este lugar en OSM</a>`
           window.bus.$emit(config.ACTIONS.TOGGLE_ALERT, title, description, footer)
         }
